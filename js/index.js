16d389ea214f3f267e0e1208f9799a1897c38ac7
tommygebru <gebru.t@gmail.com>
2016-10-22 11:11:57 -0700

    Master: First Commit.

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+    $("button").click(function() {
+        $(".big-red").toggleClass("fold");
+    //   $(".welcome").fadeToggle(1500).css("z-index", "100");
+    });
+
+});
